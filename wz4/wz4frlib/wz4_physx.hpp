fa3b28a154253bf808fbb5716239f5239d1c7872
wzman <fredakilla@gmail.com>
2013-09-02 22:08:45 +0200

    Physx fix compiler error if _DEBUG is defined

--- wz4/wz4frlib/wz4_physx.hpp
+++ wz4/wz4frlib/wz4_physx.hpp
@@ -14,23 +14,33 @@
 /****************************************************************************/
 /****************************************************************************/
 
+#ifdef _DEBUG
+#undef _DEBUG
+#define _DEBUG_WAS_DEFINED
+#endif
+
 #undef new
 #include "C:/library/PhysX-3.2.3_PC_SDK_Core/Include/PxPhysicsAPI.h"
 #define new sDEFINE_NEW
 
-// WIN32
-#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3CHECKED_x86.lib")
-#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3CommonCHECKED_x86.lib")
-#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3ExtensionsCHECKED.lib")
-#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3CookingCHECKED_x86.lib")
+#ifdef _DEBUG_WAS_DEFINED
+#undef _DEBUG_WAS_DEFINED
+#define _DEBUG
+#endif
 
-// WIN64
-/*
+#ifdef _M_X64
+// 64 bits
 #pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win64/PhysX3CHECKED_x64.lib")
 #pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win64/PhysX3CommonCHECKED_x64.lib")
 #pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win64/PhysX3ExtensionsCHECKED.lib")
 #pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win64/PhysX3CookingCHECKED_x64.lib")
-*/
+#else
+// 32 bits
+#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3CHECKED_x86.lib")
+#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3CommonCHECKED_x86.lib")
+#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3ExtensionsCHECKED.lib")
+#pragma comment(lib, "C:/library/PhysX-3.2.3_PC_SDK_Core/Lib/win32/PhysX3CookingCHECKED_x86.lib")
+#endif
 
 #ifdef _DEBUG
 #pragma comment(linker, "/NODEFAULTLIB:libcmt.lib")
